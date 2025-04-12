ccd142c4f6f8cdd278548b0994ad0901b1a3dde9
clidrobalo <csrobalo@criticalsoftware.com>
2025-04-12 17:50:18 +0100

    Add fetch-api project

---
+++ fetch-api-example/src/App.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="container p-5">
+      <h1>API Fetch</h1>
+
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
